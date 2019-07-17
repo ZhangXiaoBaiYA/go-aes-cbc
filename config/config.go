993dafda32ef2248eb55adefd815a131b435efb6
zhangxiaobai <zhangpeng@tradingchain.io>
2019-07-17 14:32:16 +0800

    go - aes -cbc

---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+var (
+	REQ_PLATFORM="tradingchain_test"
+	REQ_KEY="KHGSI69YBWGS0TWX"
+	REQ_IV="3010201735544643"
+	BASE_URL="http://localhost:8092/"
+)
---
+++ test.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "./utils"
+import "log"
+
+func main(){
+	fmt.Println("abc")
+	to_encrypt :=[]byte("i want an apple")
+	aa,e := utils.Encrypt(to_encrypt)
+	log.Println(aa)
+	log.Println(e)
+
+	//解密
+	bb,ee := utils.Decrypt(aa)
+	log.Println(bb)
+	log.Println(ee)
+}	
